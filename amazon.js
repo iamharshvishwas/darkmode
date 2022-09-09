60bd0bff72d981fc89b6eb04a38d00bc547cf79a
unknown <bishwas.harsh.000.@gmail.com>
2022-09-09 22:07:40 +0530

    code

--- amazon.js
+++ amazon.js
@@ -8,3 +8,12 @@ function black() {
     Element.classList.toggle("light-mode");
   });
 }
+function loadCoupon() {
+  document.getElementById("coupon").style.display = "block";
+  document.getElementById("search").style.opacity = "0.5";
+}
+
+const closeCoupon = () => {
+  document.getElementById("coupon").style.display = "none";
+  document.getElementById("search").style.opacity = "1";
+};
